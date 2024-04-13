22a8a5e73342685959b5f71d50bdb2b049595a4b
Angelito9827 <angelillohdez@gmail.com>
2024-04-13 19:27:48 +0200

    refactor: restyling navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { FooterComponent } from "./layouts/footer/footer.component";
+
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, AppComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, AppComponent, NavbarComponent, FooterComponent]
 })
 export class AppComponent {
 
