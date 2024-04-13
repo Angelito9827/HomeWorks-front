fb0e05187c0cad2d9e449594239238644e40933b
Angelito9827 <angelillohdez@gmail.com>
2024-04-13 21:06:06 +0200

    feature: basic routing implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TaskListComponent } from './task/task-list/task-list.component';
+import { ProfileDetailsComponent } from './profile/profile-details/profile-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", redirectTo:"home", pathMatch: 'full'},
+  {path: "home", component:HomeComponent},
+  {path: "task", component:TaskListComponent},
+  {path: "profile", component:ProfileDetailsComponent},
+];
--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -3,12 +3,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatDividerModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule,
+    MatDividerModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
